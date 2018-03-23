a70a7557b43bbe6b615a3849383665e221b17d5d
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2018-03-23 14:00:38 -0600

    initial setup

--- HelloAlerts/ViewController.swift
+++ HelloAlerts/ViewController.swift
@@ -12,14 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapSimpleAlert(_ sender: Any) {
+        print("Simple Alert")
+    }
+    
+    @IBAction func tapOkAlert(_ sender: Any) {
+        print("Ok Alert")
+    }
+    
+    @IBAction func tapOkCancelAlert(_ sender: Any) {
+        print("Ok Cancel Alert")
+    }
 }
 
